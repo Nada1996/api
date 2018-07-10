82669da81efdc66342ae7466763493285154ee30
nada <nada.abdelbaky1996@gmail.com>
2018-07-10 05:49:05 +0200

    third commit

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-import sumTwoNumbers
 import finalrecommend
 from flask import Flask, request
 from flask_restful import Resource, Api
 import pandas as pd
+import numpy as np
 from sklearn.externals import joblib
 
+happy=['Happy - Pharrell Williams','All About That Bass - Meghan Trainor','Cups -  Anna Kendrick',
+'Cyndi Lauper - Girls Just Want To Have Fun','Oasis - Live Forever','Good Vibrations - The Beach Boys',
+'The Twist - Chubby Checker','Bruno Mars - The Lazy Song','Joni Mitchell - Big Yellow Taxi','Ariana Grande - Break Free']
+sad=['Hello - Adele','Someone Like You - Adele','Im With You - Avril Lagivne','Alive - Sia','Big Girls Cry - Sia',
+'Fort Romeau - Say Something','Aly & Fila - Without You','Lana Del Rey - Is This Happiness',
+'Demi Lovato - Stone Cold','My Happy Ending - Avril Lavigne']
+
+text_clf= joblib.load('NB_model.sav')
 loaded_model = joblib.load('recommender_model.sav')
 song_df = pd.read_csv("song_df.csv")
 df = song_df.drop_duplicates(['song','user_id'])
@@ -14,6 +22,15 @@ newsong = newsong.fillna(0)
 app = Flask(__name__)
 api = Api(app)
  
+class songbymood(Resource):
+    def get(self, msg):
+        return { 'reply':finalrecommend.songbymood(text_clf,sad,happy,msg)[0], 
+        'song1': finalrecommend.songbymood(text_clf,sad,happy,msg)[1],
+        'song2': finalrecommend.songbymood(text_clf,sad,happy,msg)[2],
+        'song3': finalrecommend.songbymood(text_clf,sad,happy,msg)[3],
+        'song4': finalrecommend.songbymood(text_clf,sad,happy,msg)[4],
+        'song5': finalrecommend.songbymood(text_clf,sad,happy,msg)[5]}
+
 
 class recommend(Resource):
     def get(self, msg):
@@ -27,11 +44,13 @@ class randomsong(Resource):
         return {'song1': finalrecommend.randomsong(newsong)[0],'song2': finalrecommend.randomsong(newsong)[1],'song3': finalrecommend.randomsong(newsong)[2],
         'song4': finalrecommend.randomsong(newsong)[3],'song5': finalrecommend.randomsong(newsong)[4],'song6':finalrecommend.randomsong(newsong)[5],
         'song7': finalrecommend.randomsong(newsong)[6],'song8': finalrecommend.randomsong(newsong)[7],'song9': finalrecommend.randomsong(newsong)[8],
-        'song10': finalrecommend.randomsong(newsong)[9]}        
+        'song10': finalrecommend.randomsong(newsong)[9]}
 
 
 
 api.add_resource(recommend,'/recommend/<msg>')
+api.add_resource(songbymood,'/songbymood/<msg>')
+
 api.add_resource(randomsong,'/randomsong/')
 if __name__ == '__main__':
      app.run()
--- finalrecommend.py
+++ finalrecommend.py
@@ -2,6 +2,30 @@ import pandas as pd
 from sklearn.externals import joblib
 import numpy as np
 
+happy=['Happy - Pharrell Williams','All About That Bass - Meghan Trainor','Cups -  Anna Kendrick',
+'Cyndi Lauper - Girls Just Want To Have Fun','Oasis - Live Forever','Good Vibrations - The Beach Boys',
+'The Twist - Chubby Checker','Bruno Mars - The Lazy Song','Joni Mitchell - Big Yellow Taxi','Ariana Grande - Break Free']
+sad=['Hello - Adele','Someone Like You - Adele','Im With You - Avril Lagivne','Alive - Sia','Big Girls Cry - Sia',
+'Fort Romeau - Say Something','Aly & Fila - Without You','Lana Del Rey - Is This Happiness',
+'Demi Lovato - Stone Cold','My Happy Ending - Avril Lavigne']
+
+def songbymood(text_clf,sad,happy,msg):
+	#print('hey there')
+	listsongs=[]
+	
+	predicted = text_clf.predict([msg])
+	if predicted[0]=='JOY':
+		listsongs.append('Smile and shine, Heres something that will make you more happy' )
+		for i in range(5):
+			query_index = np.random.choice(len(happy))
+			listsongs.append(happy[query_index])
+	if predicted[0]=='SADNESS':
+		listsongs.append('Uhhh oh, I see some sad vibes, heres something you need for now' )
+		for i in range(5):
+			query_index = np.random.choice(len(sad))
+			listsongs.append(sad[query_index])
+
+	return listsongs
 def randomsong(newsong):
 	listsongs=[]
 	for i in range(10):
